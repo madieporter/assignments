7fd73e025890a5da9964f19bf11e54d0db4b49e5
Madie Porter <madieporter@gmail.com>
2019-03-09 10:26:04 -0700

    push name

--- projects/personal-react-site/src/NavBar.js
+++ projects/personal-react-site/src/NavBar.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import home from "./home.png";
+import cheers from "./cheers.png";
+import forkKnife from "./forkKnife.png";
+
 const NavBar = () => {
     return (
         <div>
             <div className="navBar">
-                <Link to="/">home</Link>
-                <Link to="/allbeers">beers</Link>
-                <Link to="/featured">featured</Link>
+                <div className="navContainer">
+                    <Link to="/"><img className="navLogo" src={home} alt=""/></Link>
+                    <Link to="/allbeers"><img className="navLogo" src={cheers} alt=""/></Link>
+                    <Link to="/featured"><img className="navLogo" src={forkKnife} alt=""/></Link>
+                </div>
             </div>
         </div>
     )
